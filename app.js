f29d042ee84cf95d66012d396e08d0474be7443a
Akriti Sondhi <akriti.sondhi@metacube.com>
2020-06-09 22:15:05 +0530

    Set up modules before starting the project

---
+++ app.js
@@ -0,0 +1,14 @@
+// Budget Controller
+var budgetController = (function () {
+  return {};
+})();
+
+// UI Controller
+var UIController = (function () {})();
+
+//Global App Controller
+var appController = (function (_budgetController, _UIController) {
+  return {};
+})(budgetController, UIController);
+
+appController.secondPublic();
